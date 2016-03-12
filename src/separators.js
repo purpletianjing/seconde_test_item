afb31727a25a00595243bcd8481432bdadc406c2
purpletianjing <TianjingSun@outlook.com>
2016-03-12 10:09:56 +0800

    complete the question

---
+++ src/separators.js
@@ -0,0 +1,12 @@
+'use strict';
+
+function thousands_separators(n) {
+  // x = x.toString();
+  // var pattern = /(-?\d+)(\d{3})/;
+  // while (pattern.test(x))
+  //     x = x.replace(pattern, "$1,$2");
+  var parts=n.toString().split(".");
+    return parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",") + (parts[1] ? "." + parts[1] : "");
+}
+
+module.exports = thousands_separators;
